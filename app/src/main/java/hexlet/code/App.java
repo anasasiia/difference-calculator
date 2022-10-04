30ff7abcbafed05a951a6566ec01e7f2bcd8fdca
anasasiia <sheldoncooperbuga@gmail.com>
2022-10-04 12:41:36 +0400

    build makefile and picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.nio.file.Files;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", version = "gendiff 1.0",mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return null;
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+
 }
